7a3d435ce9669c24591fe3265377cfa4df960ad7
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-02-19 02:01:31 +0530

    Eighth Commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@ window.onscroll = function() {
   if(prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-100px";
+    document.getElementById("navbar").style.top = "-76px";
   }
   prevScrollpos = currentScrollPos;
 }
