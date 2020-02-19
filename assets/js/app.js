f276d087da1ac343ddf180b1f346d47569917dec
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-02-19 16:50:39 +0530

    Thirteenth Commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,24 @@
-var prevScrollpos = window.pageYOffset;
+var nav = document.getElementsByTagName("nav")[0];
+
+window.addEventListener("load", function(){
+  var loader = document.querySelector(".loader");
+
+  loader.className += " hidden";
+  nav.classList.remove("hide");
+  nav.classList.add("show");
+});
+
+var prevScrollpos = window.pageYOffset,
+    button        = document.getElementsByClassName("version")[0],
+    section       = document.getElementsByTagName("section")[0];
+
+button.addEventListener("click", function(){
+  section.classList.toggle("section-1");
+  section.classList.toggle("section-2");
+});
 
 window.onscroll = function() {
+  nav.classList.remove("show");
   var currentScrollPos = window.pageYOffset;
   if(prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
